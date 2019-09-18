1b3a7c11f60e72f3cfd069bd1382d566bcd2ff32
github username <github email address>
2019-09-18 10:01:41 -0700

    done

--- src/index.js
+++ src/index.js
@@ -3,4 +3,22 @@ console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate 
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
+  document.getElementById("but").addEventListener("click", function() {
+    let data = fetch("https://randomuser.me/api/").then(function(response){return response.json()}).then(function(json){
+  let info = json.results[0];
+  console.log(info);
+  document.getElementById("profile_picture").src = json.results[0].picture.thumbnail;
+  document.getElementById("fullname").textContent = info.name.first + " " + info.name.last;
+  document.getElementById("email").textContent = info.email;
+  document.getElementById("street").textContent = info.location.street;
+  document.getElementById("city").textContent = info.location.city;
+  document.getElementById("state").textContent = info.location.state;
+  document.getElementById("postcode").textContent = info.location.postcode;
+  document.getElementById("phone").textContent = info.phone;
+  document.getElementById("cell").textContent = info.cell;
+  document.getElementById("date_of_birth").textContent = info.dob.date.split("T")[0];
 });
+  });
+});
+function summonPerson() {
+}
